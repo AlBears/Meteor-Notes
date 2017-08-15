904f0565b13c9213f96771ac1ff91199edacf0c0
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-08-15 20:03:21 +0300

    test publications

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -12,10 +12,18 @@ if (Meteor.isServer) {
       updatedAt: 0,
       userId: 'testUserId1'
     };
+    const noteTwo = {
+      _id: 'testNoteId2',
+      title: 'Things to buy',
+      body: 'Couch',
+      updatedAt: 0,
+      userId: 'testUserId2'
+    };
 
     beforeEach(function() {
       Notes.remove({});
       Notes.insert(noteOne);
+      Notes.insert(noteTwo);
     });
 
     it('should insert new note', function () {
@@ -103,6 +111,21 @@ if (Meteor.isServer) {
       expect(() => {
         Meteor.server.method_handlers['notes.update'].apply({userId: noteOne.userId});
       }).toThrow();
+    });
+
+    it('should return users notes', function() {
+      const res = Meteor.server.publish_handlers.notes.apply({ userId: noteOne.userId });
+      const notes = res.fetch();
+
+      expect(notes.length).toBe(1);
+      expect(notes[0]).toEqual(noteOne);
+    });
+
+    it('should return no notes for user that has none', function() {
+      const res = Meteor.server.publish_handlers.notes.apply({ userId: 'testId' });
+      const notes = res.fetch();
+
+      expect(notes.length).toBe(0);
     });
   });
 }
